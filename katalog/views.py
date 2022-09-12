d4227a17eb8cc973390ace6a2824b296afe9923c
carissa.almira <carissa.almira@ui.ac.id>
2022-09-12 11:20:47 +0700

    Done App Catalogs

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+def show_catalog(request):
+    data_barang_catalog = CatalogItem.objects.all()
+    context = {
+    'list_barang': data_barang_catalog,
+    'nama'      : 'Carissa Almira',
+    'studentid' : '2106751676',
+}
+    return render(request, "katalog.html", context)
