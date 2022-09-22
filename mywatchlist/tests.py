a9816fcc0a99d3ea949870a38378cab84becc1cd
carissa.almira <carissa.almira@ui.ac.id>
2022-09-22 08:37:13 +0700

    Done Tugas 3

--- mywatchlist/tests.py
+++ mywatchlist/tests.py
@@ -1,3 +1,27 @@
 from django.test import TestCase
 
 # Create your tests here.
+class WatchListUnitTesting(TestCase):
+    def setUp(self):
+        self.client = Client()
+
+    def view_url(self):
+        response = self.client.get('/mywatchlist/')
+        self.assertEqual(response.status_code,200)
+
+    def view_template(self):
+        response = self.client.get('/mywatchlist/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'mywatchlist.html')
+
+    def test_view_html(self):
+        response = self.client.get('/mywatchlist/html/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_xml(self):
+        response = self.client.get('/mywatchlist/xml/')
+        self.assertEqual(response.status_code, 200)
+
+    def test_view_json(self):
+        response = self.client.get('/mywatchlist/json/')
+        self.assertEqual(response.status_code, 200)
