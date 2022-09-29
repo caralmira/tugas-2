bca54f29990da5fd0f8ad51a2e6bd8173b05439b
carissa.almira <carissa.almira@ui.ac.id>
2022-09-29 08:11:40 +0700

    Tugas 4

--- todolist/views.py
+++ todolist/views.py
@@ -5,7 +5,6 @@ from django.shortcuts import render
 from django.shortcuts import redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
-from todolist.apps import TodolistConfig
 from urllib.parse import parse_qs
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
